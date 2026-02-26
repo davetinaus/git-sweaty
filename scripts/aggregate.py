2af2bb5d2088877d234938b1053c54232f351ec8
davetinaus <96107480+davetinaus@users.noreply.github.com>
2026-02-26 16:13:23 +1100

    Update aggregate.py

--- scripts/aggregate.py
+++ scripts/aggregate.py
@@ -23,7 +23,7 @@ def aggregate():
         activity_type = item.get("type")
         if activity_type in exclude_types:
             continue
-        if hidden = false:
+        if hidden == false:
             continue
         if not include_all_types and featured_types and activity_type not in featured_types:
             continue
